415edacbd192be7b2ae8bce149cdfd714536f0e1
Pedro C <46518927+thepedrocalzado@users.noreply.github.com>
2021-01-16 23:50:38 +0100

    Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import '../App.css';
+import '../index.css';
+import home from "../img/navbar/home.png";
+import news from "../img/navbar/proximamente.png";
+import search from "../img/navbar/lupa.png";
+import download from "../img/navbar/descargas.png";
+
+
+function NavBar() {
+    return (
+        <div className="w-full grid grid-cols-4 items-center absolute bg-gray-900 sticky bottom-0">
+               <div className="col-span-1 m-2 text-center">
+                   <img className="h-6 mx-auto mb-1" src={home}/>
+                   <p className="text-white text-xs">Inicio</p>
+               </div>
+
+            <div className="col-span-1 m-2 text-center">
+                    <img className="h-6 mx-auto mb-1" src={news}/>
+                    <p className="text-gray-500 text-xs">Próximamente</p>
+                </div>
+
+            <div className="col-span-1 m-2 text-center">
+                    <img className="h-6 mx-auto mb-1" src={search}/>
+                    <p className="text-gray-500 text-xs">Búsqueda</p>
+                </div>
+
+            <div className="col-span-1 m-2 text-center">
+                    <img className="h-6 mx-auto mb-1" src={download}/>
+                    <p className="text-gray-500 text-xs">Descargas</p>
+                </div>
+        </div>
+
+
+    );
+}
+
+export default NavBar;
